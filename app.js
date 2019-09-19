f4242f96ebd0a6636b658410f81b1ae61fc2d63c
krushnap <smitakpatil26@gmail.com>
2019-09-19 16:39:04 +0530

    design updated

--- app.js
+++ app.js
@@ -1,26 +1,45 @@
-function myfun()
-{
-            myGameArea.start();
+
+    let c=document.getElementById("game-space");
+    c.height=window.innerHeight;
+    c.width=window.innerWidth;
+    let ctx=c.getContext("2d");
+                
+function Circle(r,y,dy){
+        this.r=r;
+        this.y=y;
+        this.dy=dy;
+        this.draw=function(){
+        ctx.beginPath();
+        ctx.arc(200,this.y,this.r,0,2*Math.PI);
+        ctx.fillStyle="#DAE0E2";
+        ctx.fill();
+    }
+    this.update=function(){
+        this.y-=this.dy; 
+        this.draw();
+        console.log("draaw")
+    }
 }
 
-var myGameArea = {
-        start:function(){
-                let c=document.getElementById("game-space");
-                // let c=document.querySelector("canvas")
-                c.height=window.innerHeight;
-                c.width=window.innerWidth;
-                let ctx=c.getContext("2d");
-                let i;
-                for(i=0;i<120;i++)
-                {
-                    let x=Math.floor(Math.random()*c.width);
-                    let y=Math.floor(Math.random()*c.height);
-                    console.log(x+""+y)
-                    ctx.beginPath();
-                    ctx.arc(x,y,10,0,2*Math.PI);
-                    ctx.fillStyle="#DAE0E2";
-                    ctx.fill();
-                }
-        }   
-}
-      
+
+let circleArray=[];
+let r=Math.floor(Math.random()*30);
+
+
+console.log(circleArray)
+function animate(){
+        requestAnimationFrame(animate);
+        for(let i=0;i<40;i++)
+        {
+            circleArray.push(new Circle(r,700,1))
+    
+        }
+        ctx.clearRect(0,0,innerWidth,innerHeight)
+        
+        for(let i=0;i<40;i++)
+        {
+            console.log(circleArray[i])
+            circleArray[i].update();
+        }
+}     
+animate();
